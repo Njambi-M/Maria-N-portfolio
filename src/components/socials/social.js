25d53f6f86f4d2b0c4a983f0e7478717e3e11f81
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-01-29 01:41:06 +0300

    feat: add the social media links component

---
+++ src/components/socials/social.js
@@ -0,0 +1,14 @@
+import "./social.css";
+
+export default function Social() {
+    return (
+<div class="home_social">
+                    <a href="https://www.linkedin.com/in/maria-muthiore" target="_blank" class="social_icon">
+                        <i class="uil uil-linkedin"></i>
+                    </a>
+                    <a href="https://github.com/Njambi-M" target="_blank" class="social_icon">
+                        <i class="uil uil-github"></i>
+                    </a> 
+            </div> 
+    );
+}
