684a31b6db8700e177cbc71065ee3dd76e4720a1
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-01-29 02:53:23 +0300

    feat: add skills component to the App.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './components/header/header';
 import Home from './components/home/home';
+import Skills from './components/skills/skills';
 
 function App() {
   return (
    <>
   <Header/>
   <Home/>
+  <Skills/>
   </>
   );
 }
