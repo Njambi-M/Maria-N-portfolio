a21aa7aef18575cf6f48a9194762a992b8420f5d
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-01-28 23:44:25 +0300

    feat(components): add header section

---
+++ src/components/header/header.js
@@ -0,0 +1,16 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div class="header">
+            <span class="name">Maria Njambi Muthiore</span>
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#skills">Skills</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#contact">Contact</a></li>
+
+                </ul>   
+    </div>
+  );
+}
