3317758cc53d23230a0263b089680c699b4c6d09
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-01-29 02:15:14 +0300

    refactor(home): add the noreferrer keyword to the rel attribute of the download button

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -6,7 +6,7 @@ export default function Home() {
     <section class = "home section" id ="home">
                 <img src="assets\Photo2.jpg" alt="Me" class="home_img"/>
                 <h1 class="home_title">Maria<br/> Software Developer</h1>
-                <a href="assets\Resume.pdf" download="Resume.pdf" class="download-button" target="_blank">Download Resume</a>
+                <a href="assets\Resume.pdf" download="Resume.pdf" class="download-button" target="_blank" rel='noreferrer'>Download Resume</a>
 
                 <p class="home_intro">Knowledge of various programming languages and software development methodologies.<br/>
                     I enjoy problem-solving, testing my skills, and continually seeking ways to improve. <br/> 
