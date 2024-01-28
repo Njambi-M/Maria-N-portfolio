7816c59d7b7a4ee72559b745b3b32fc4363105c2
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-01-29 02:58:35 +0300

    refactor(skills): add the noreferrer keyword to the rel attribute of the links

--- src/components/socials/social.js
+++ src/components/socials/social.js
@@ -3,10 +3,10 @@ import "./social.css";
 export default function Social() {
     return (
 <div class="home_social">
-                    <a href="https://www.linkedin.com/in/maria-muthiore" target="_blank" class="social_icon">
+                    <a href="https://www.linkedin.com/in/maria-muthiore" target="_blank" rel='noreferrer' class="social_icon">
                         <i class="uil uil-linkedin"></i>
                     </a>
-                    <a href="https://github.com/Njambi-M" target="_blank" class="social_icon">
+                    <a href="https://github.com/Njambi-M" target="_blank" rel='noreferrer' class="social_icon">
                         <i class="uil uil-github"></i>
                     </a> 
             </div> 
