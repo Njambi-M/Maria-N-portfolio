bd8582b1505b1f430067251bac7fc16eeaf9f285
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-01-29 20:55:22 +0300

    refactor: add section in contacts component

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -4,6 +4,7 @@ import React from 'react';
 export default function Contact() {
     return(
         <>
+        <section className = "contact section" id ="contact">
         <h1>Contact Me</h1>
         <div className="contact-info">
             <div className="contact-details">
@@ -31,6 +32,7 @@ export default function Contact() {
                 </form>
             </div>
         </div>
+    </section>
             </>
     );
 }
