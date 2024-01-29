9ec05b3f044fb9e8a6f727792fc2411ea10769b9
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-01-29 21:34:33 +0300

    refactor(contacts): change title name

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -5,7 +5,7 @@ export default function Contact() {
     return(
         <>
         <section className = "contact section" id ="contact">
-        <h1>Contact Me</h1>
+        <h1 className="contact_title">Contact Me</h1>
         <div className="contact-info">
             <div className="contact-details">
                 <div className="contacts">
