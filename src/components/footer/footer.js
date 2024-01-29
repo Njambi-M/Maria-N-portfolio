a65ef47e450c21580c4fe9532f1e3ecf5785ecb7
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-01-29 20:24:34 +0300

    fix(footer): change class to className

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function Footer() {
     return(
 
-        <footer class="footer">
+        <footer className="footer">
             <p>&copy; 2024 All Rights Reserved, Maria Njambi</p>
         </footer>
     );
