142364aee1d05fefece2bb9e7fa2a35ec52e82a9
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-01-29 20:39:55 +0300

    refactor(home): link resume to download button

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,5 +1,6 @@
 import Social from "../socials/social";
 import myImage from '../../assets/Photo2.jpg'
+import resume from '../../assets/Resume.pdf'
 import "./home.css";
 
 export default function Home() {
@@ -7,7 +8,7 @@ export default function Home() {
     <section className = "home section" id ="home">
                 <img src={myImage} alt="Me" className="home_img"/>
                 <h1 className="home_title">Maria<br/> Software Developer</h1>
-                <a href="assets\Resume.pdf" download="Resume.pdf" className="download-button" target="_blank" rel='noreferrer'>Download Resume</a>
+                <a href={resume} download="Resume.pdf" className="download-button" target="_blank" rel='noreferrer'>Download Resume</a>
 
                 <p className="home_intro">Knowledge of various programming languages and software development methodologies.<br/>
                     I enjoy problem-solving, testing my skills, and continually seeking ways to improve. <br/> 
