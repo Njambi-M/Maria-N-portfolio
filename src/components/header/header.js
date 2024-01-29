71cad7a5592278dbc4e0ab3d99f35ee45fff1113
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-01-29 20:47:11 +0300

    refactor: link navigation component to header component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,16 +1,11 @@
+import Navigation from "../navigation/navigation";
 import "./header.css";
 
 export default function Header() {
   return (
     <div className="header">
             <span className="name">Maria Njambi Muthiore</span>
-                <ul>
-                    <li><a href="#home">Home</a></li>
-                    <li><a href="#skills">Skills</a></li>
-                    <li><a href="#projects">Projects</a></li>
-                    <li><a href="#contact">Contact</a></li>
-
-                </ul>   
+            <Navigation/>
     </div>
   );
 }
