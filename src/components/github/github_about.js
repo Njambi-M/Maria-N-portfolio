ab8831da0777cb9fdac354bab4c1319f1e3bc764
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-01-29 22:02:16 +0300

    feat: add Github component to fetch data from Github API

---
+++ src/components/github/github_about.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+
+export default function GithubAbout() {
+    const [userData, setUserData] = useState({});
+    const [repoCount, setRepoCount] = useState(0);
+    const username = 'Njambi-M';
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await fetch(`https://api.github.com/users/${username}`);
+            const result = await response.json();
+            setUserData(result);
+            const repoResult = await repoResponse.json();
+            setRepoCount(repoResult.length);
+          } catch (error) {
+            console.error('Error fetching GitHub data:', error);
+          }
+        };
+    
+        fetchData();
+      }, [username]);
+
+    return(  
+    <div className="git_info">
+      <h2>{`GitHub User: ${userData.login}`}</h2>
+      <p>{`Name: ${userData.name}`}</p>
+      <p>{`Number of Repositories: ${repoCount}`}</p>
+    </div>
+  );
+};
