64aebe90634f5e4ff6ce3d3c26d36ef11db9d20b
Njambi-M <100689705+Njambi-M@users.noreply.github.com>
2024-01-29 19:17:40 +0300

    feat: add the projects component to the App.js file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './components/header/header';
 import Home from './components/home/home';
+import Projects from './components/projects/projects';
 import Skills from './components/skills/skills';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   <Header/>
   <Home/>
   <Skills/>
+  <Projects/>
   </>
   );
 }
